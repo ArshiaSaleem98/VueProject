9fdf253a8c44b95b822948396679a28352a17de2
ArshiaSaleem98 <arshiasaleem98@gmail.com>
2023-06-22 01:29:20 +0100

    feat:add-url-file&Edit-user-service

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const port = window.location.port;
+
+const BASE_URL = `http://localhost:${port}/webdev/api/users`; // Replace with your actual base URL
+
+export default BASE_URL;
---
+++ src/services/EditUserService.js
@@ -0,0 +1,20 @@
+import BASE_URL from '@/config';
+import axios from 'axios';
+
+const EditUserService = {
+  async editUser(id, user) {
+    const timestamp = new Date().getTime();
+
+    try {
+      return await axios.put(
+        BASE_URL + '/' + id + `?timestamp=${timestamp}`,
+        user
+      );
+    } catch (error) {
+      console.error('Error Editing the user:', error);
+      throw error;
+    }
+  },
+};
+
+export default EditUserService;
